384d068259acc2e8d2a2b78aeccc5780649defc7
geraldclarkaudio <geraldclarkaudio@gmail.com>
2021-10-09 23:28:53 -0500

    app is done

---
+++ Assets/The Game/Scripts/CardView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CardView : MonoBehaviour
+{
+    public Text title;
+    public Text description;
+    public Text inExhibit;
+    public Image animalImg;
+
+    public CardModel[] cards;
+
+    public void DisplayCard(int card)
+    {
+        title.text = cards[card].title;
+        description.text = cards[card].description;
+        inExhibit.text = cards[card].exhibit;
+        animalImg.sprite = cards[card].animalImg;
+    }
+}
